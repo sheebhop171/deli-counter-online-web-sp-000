e2672d6d926a9e266617e4a2d2ed69bb5ceccf44
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-21 19:43:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else katz_deli != []
-    puts "The line is currenct #{katz_deli}"
+    puts "The line is currently: #{katz_deli}"
   end
 end
