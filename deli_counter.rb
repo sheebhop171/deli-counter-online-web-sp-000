c9b82114c282933d09d757318b589103d56b7c05
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-21 20:07:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,10 @@
 # Write your code here.
-customer1 = []
-this_line = "The line is currently: "
 
 def line(katz_deli)
+
+  customer1 = []
+  this_line = "The line is currently: "
+
   if katz_deli.empty?
     puts "The line is currently empty."
   else katz_deli.each_with_index(1) do |name, index|
